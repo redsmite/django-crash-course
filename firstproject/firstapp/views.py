cb19f1fd75230518ac43e37e4e517614b3e813f3
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-04-21 17:37:38 +0800

    fixed views

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+def hello_world(request): # Ensure this is spelled correctly now too!
+    return HttpResponse("Hello world")
+
+class HelloEthiopia(View): # Check this spelling!
+    def get(self, request):
+        return HttpResponse("Hello Ethiopia")
