a383f86fae9541dfc0c77956aa939125b6d852d8
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-04-22 10:15:24 +0800

    add template

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 
 def hello_world(request): # Ensure this is spelled correctly now too!
     return HttpResponse("Hello world")
@@ -8,3 +9,14 @@ def hello_world(request): # Ensure this is spelled correctly now too!
 class HelloEthiopia(View): # Check this spelling!
     def get(self, request):
         return HttpResponse("Hello Ethiopia")
+    
+def home(request):
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Success")
+        
+    return render(request, 'index.html', {'form' : form})
